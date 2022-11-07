9bf05a950b76b65aeaa25a19480a27858793c30b
agiaya <87878326+agiaya@users.noreply.github.com>
2022-11-06 20:42:22 -0500

    pca

---
+++ pca_analysis.py
@@ -0,0 +1,7 @@
+# running PCA Analysis on the dimensions from the data set
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
