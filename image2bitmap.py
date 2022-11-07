7cc1dc992600ea2537d7eabb3c2a403a123cc45f
agiaya <87878326+agiaya@users.noreply.github.com>
2022-11-06 20:09:21 -0500

    im 2 bitmap

---
+++ image2bitmap.py
@@ -0,0 +1,28 @@
+# Python image to bitmap script
+
+import numpy as np
+from skimage import io 
+from skimage.transform import resize
+import os
+
+path = os.getcwd()
+
+#main()
+
+def transformImage(file_path,file_name,extension=None):
+	image = io.imread(file_name, as_gray = True)
+	image = resize(image, (28,28))
+	array = np.array(image)
+	array = np.multiply(array,255)
+	array = np.rint(array)
+	np.savetxt("test_file.txt", array)
+
+
+def main():
+	transformImage(path, "image1", None)
+
+transformImage(path,'image1.jpg',None)
+
+#image = resize(image, (28, 28), mode='nearest')
+#array = np.array(image)
+#np.savetxt("file.txt", array, fmt="%d")
