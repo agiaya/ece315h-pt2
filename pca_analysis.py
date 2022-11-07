c60d9cc3d9227d2b0af81413625755eea825ff42
agiaya <87878326+agiaya@users.noreply.github.com>
2022-11-06 21:05:54 -0500

    plot

--- pca_analysis.py
+++ pca_analysis.py
@@ -19,3 +19,8 @@ x = principal.transform(Scaled_data)
 
 print(x)
 #print(x.shape)
+
+plt.figure(figsize=(10,10))
+plt.scatter(x[:,0],x[:,1],c=data['target'],cmap='plasma')
+plt.xlabel('pc1')
+plt.ylabel('pc2')
