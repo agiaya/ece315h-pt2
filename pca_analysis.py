6b8b00c542c22bc5b85594a71d8733fb7f1c836e
agiaya <87878326+agiaya@users.noreply.github.com>
2022-11-06 21:32:07 -0500

    reiuwhfiuewhf
    jefl

--- pca_analysis.py
+++ pca_analysis.py
@@ -6,12 +6,14 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from read_csv import Database
 
-data = Database("training_short").return_array()
+data = Database("training_short")
+data_array = data.return_array()
+data_result = data.return_result()
 
 scaling = StandardScaler()
 
-scaling.fit(data)
-Scaled_data = scaling.transform(data)
+scaling.fit(data_array)
+Scaled_data = scaling.transform(data_array)
 
 principal = PCA(n_components = 3)
 principal.fit(Scaled_data)
@@ -21,7 +23,7 @@ print(x)
 #print(x.shape)
 
 plt.figure(figsize=(10,10))
-plt.scatter(x[:,0],x[:,1],c=data.return_result(),cmap='plasma')
+plt.scatter(x[:,0],x[:,1],c=data_result,cmap='plasma')
 plt.xlabel('pc1')
 plt.ylabel('pc2')
 plt.show()
